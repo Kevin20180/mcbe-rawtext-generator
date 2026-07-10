e5a7d960554c3e12928718b0fe17d4aeba5418e0
Kevin <kevinlucas7910@gmail.com>
2026-07-09 20:14:46 -0400

    feat: adiciona definições de RawText

---
+++ src/rawtext.ts
@@ -0,0 +1,16 @@
+export interface RawText {
+	rawtext: RawMessage[]
+}
+
+export interface RawMessage {
+    rawtext?: RawMessage[];
+    score?: RawMessageScore;
+    text?: string;
+    translate?: string;
+    with?: string[] | RawMessage;
+}
+
+export interface RawMessageScore {
+    name?: string;
+    objective?: string;
+}
