469ad9a08222b98d51f2843d54160bf029574e8a
Kevin <kevinlucas7910@gmail.com>
2026-07-09 20:31:34 -0400

    feat: adiciona REPL que permite digitar textos e mostrar resultado em rawtext

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export {}
+import * as readline from 'readline';
+import { lexer } from './lexer.js';
+import { parse } from './parser.js';
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
+
+console.log('MCBE RawText Generator v0.1.0');
+console.log('Enter text below to convert it to rawtext.');
+
+rl.on('line', (line) => {
+	let tokens = lexer(line);
+	let result = parse(tokens);
+
+	console.log(JSON.stringify(result));
+	process.stdout.write('> ');
+})
+
+process.stdout.write('> ');
